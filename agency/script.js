677c850c17e5f4977de5e18d42a10c6a7844df6b
Marcin Jablonowski <marcin.jab@wp.pl>
2018-02-19 14:26:57 +0100

    Banner Css

--- agency/script.js
+++ agency/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#menu-btn').click(changeNav);
+    $('#menu-btn').click(changeNav)
     function changeNav(){
         $('#links').toggle(1000);
         $('#menu-btn').toggleClass('turn');
