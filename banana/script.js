2c6a5acc0d9933cbffeffbd5eb61b808166f330f
Marcin Jablonowski <marcin.jab@wp.pl>
2018-02-22 22:33:19 +0100

    Skills section

--- banana/script.js
+++ banana/script.js
@@ -1,23 +1,24 @@
-$(document).ready(function(){
-    $('#toggle-btn').click(function(){
+$(document).ready(function () {
+    $('#toggle-btn').click(function () {
         $('.nav-links').slideToggle(3000);
     })
 })
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     let scroll = $(window).scrollTop();
-    if(scroll>=70){
+    if (scroll >= 70) {
         $('#nav').addClass('new-nav');
     } else {
         $('#nav').removeClass('new-nav');
     }
 })
 
-$('nav a').click(function(link){
+$('nav a').click(function (link) {
     link.preventDefault();
 
     let target = $(this).attr('href');
 
     $('html, body').animate({
-        scrollTop: $(target).ofset().top}, 'slow')
+        scrollTop: $(target).ofset().top
+    }, 'slow')
 })
