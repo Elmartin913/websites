6f2cc3e14c63fe3314855883641a9b891bc69a29
Marcin Jablonowski <marcin.jab@wp.pl>
2018-02-22 11:31:46 +0100

    Additional Nav Feature

--- banana/script.js
+++ banana/script.js
@@ -3,3 +3,21 @@ $(document).ready(function(){
         $('.nav-links').slideToggle(3000);
     })
 })
+
+$(window).scroll(function(){
+    let scroll = $(window).scrollTop();
+    if(scroll>=70){
+        $('#nav').addClass('new-nav');
+    } else {
+        $('#nav').removeClass('new-nav');
+    }
+})
+
+$('nav a').click(function(link){
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(target).ofset().top}, 'slow')
+})
