b54d32d4bf2e15841882d1e25fe3763fa117ee14
Marcin Jablonowski <marcin.jab@wp.pl>
2018-02-24 13:03:45 +0100

    Current section

--- banana/iso.js
+++ banana/iso.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     // init Isotope
     var $grid = $('.picture-container').isotope({
-    // options
-    });
-    // filter items on button click
-    $('.filter-button-group').on( 'click', 'button', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ 
-            filter: filterValue 
+        // options
         });
-    });
-    // layout Isotope after each image loads
-    $grid.imagesLoaded().progress( function() {
-        $grid.isotope('layout');
+        // filter items on button click
+        $('.filter-button-group').on( 'click', 'button', function() {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ 
+                filter: filterValue 
+            });
+        });
+        // layout Isotope after each image loads
+        $grid.imagesLoaded().progress( function() {
+            $grid.isotope('layout');
     });
 });
