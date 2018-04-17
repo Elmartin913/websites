aaae6c51be3e7e136c478c14ba2632a8f9e65145
Marcin Jablonowski <marcin.jab@wp.pl>
2018-04-17 18:10:07 +0200

    Readme

--- pandjango/env-pandjango/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/elmartin913/workspace/app/pandjango/env-pandjango/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- pandjango/pandjango/website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.views import View
-from django.template.response import TemplateResponse
-# Create your views here.
-
-class StartView(View):
-    def get(self, request):
-        return TemplateResponse(request, 'index.html')
